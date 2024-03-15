1b2b35a68154258bcc039d9e6103b3d25129fcb1
Anuroop T <88581969+iam-anuroop@users.noreply.github.com>
2024-03-15 22:38:57 +0530

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     ManageGlobalGroup,
     GetGlobalGrroups,
-    SearchRandomPerson
+    SearchRandomPerson,
+    BlockUserFromGroup,
 )
 
 urlpatterns = [
